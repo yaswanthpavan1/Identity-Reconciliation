27236ab904e57b4721edea5d8551145a34b4198f
yaswanthpavan1 <150510469+yaswanthpavan1@users.noreply.github.com>
2024-11-10 01:28:39 +0530

    Add files via upload

---
+++ migrations/20241107081720-create-contact.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      linkedId: {
+        type: Sequelize.INTEGER
+      },
+      linkPrecedence: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contacts');
+  }
+};
